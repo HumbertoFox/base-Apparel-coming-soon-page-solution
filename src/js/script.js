99dec2b5979a68a6798f74b7ada4246b92a5bcc3
Humberto Sales <betofoxmal@gmail.com>
2023-11-30 21:46:36 -0300

    update finalização js e css

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,32 @@
 const selectDivInput = document.querySelector(".div-input");
-const selectInputEmaail = document.getElementById("input-email");
+const selectInputEmail = document.getElementById("input-email");
 
+document.getElementById("btn-email").addEventListener("click", () => {
+    const checkdEmail = selectInputEmail.value;
+    const insertPText = '<p class="error-message">Please provide a valid email</p>';
+
+    if (validateEmail(checkdEmail) === true) {
+        selectInputEmail.value = "";
+        removeTagP();
+    } else {
+        removeTagP();
+        selectInputEmail.classList.add("error");
+        selectDivInput.insertAdjacentHTML("afterend", insertPText);
+    };
+});
+
+function removeTagP() {
+    const classError = selectInputEmail.classList.value;
+
+    if (classError.includes("error")){
+        selectInputEmail.classList.remove("error");
+        selectDivInput.nextElementSibling.remove("error-message");
+    } else {
+        return;
+    };
+};
+
+const validateEmail = (email) => {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+};
